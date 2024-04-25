b2d1bee286f8d03047ce7b841a34af3ffe735180
Abdulkadir_USTA <usta1829@gmail.com>
2024-04-25 15:45:42 +0300

    php eklendi.

---
+++ php/index.php
@@ -0,0 +1,11 @@
+<?php 
+  
+   echo"AD:".$_POST['AD']."<br><br>";
+   echo"SOYAD:".$_POST['SOYAD']."<br><br>";
+   echo"TELEFONNO:".$_POST['TEL']."<br><br>";
+   echo"E-MAİLADRESİ:".$_POST['MAİL']."<br><br>";
+   echo"ADRES:".$_POST['ADRES']."<br><br>";
+   echo"İLETİLENMESAJ:".$_POST['MESAJ']."<br><br>";
+   echo "---------------------------------------------------------------------------" ;  
+ 
+?>
---
+++ php/index2.php
@@ -0,0 +1,9 @@
+<?php 
+  $kullaniciadi=$_POST['kullaniciadi'];
+  $ekranciktisi=substr($kullaniciadi,0,10);
+
+  echo"HOŞGELDİNİZ  ". $ekranciktisi."<br><br>";
+ 
+    
+ 
+?>
